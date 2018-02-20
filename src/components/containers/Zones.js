c5be068f10a9c346cc567e3d270bdfc7a62216cc
okputadora <okputadora@gmail.com>
2018-02-20 02:49:51 -0500

    update view via api request to db

--- src/components/containers/Zones.js
+++ src/components/containers/Zones.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Zone from '../presentation/Zone'
+import superagent from 'superagent'
+
 class Zones extends Component {
   constructor(){
     super()
@@ -10,6 +12,29 @@ class Zones extends Component {
       },
       list: []
     }
+  }
+  // this is another built in function that we are overriding
+  componentDidMount(){
+    console.log('componentDidMount')
+
+    superagent
+    // give it a url
+    .get('/api/place')
+    // parameters if we had any
+    .query(null)
+    .set('Accept', 'application/json')
+    .end((err, response) => {
+      if (err){
+        alert('error '+err)
+        return
+      }
+      console.log(JSON.stringify(response.body.results))
+      let results = response.body.results
+      this.setState({
+        list: results
+      })
+
+    })
   }
   // different way from doing the comments one - putting it all in one function
   updateZone(event){
